cd313f9b009bb0c290d9049832c3c2d54d380485
Gowtham-Gtm <165893161+Gowtham-Gtm@users.noreply.github.com>
2024-11-26 22:11:37 +0530

    first

---
+++ project/eventpr/eventapp/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from.models import Makeup,Decorator,Invitation,Catering,Gift,Wear,Jewellery,Photographer,Hotel,Choreographer,Mehendi,Dj
+from.models import Booking_mak,Booking_dec,Booking_inv,Booking_cat,Booking_gif,Booking_wea,Booking_jew,Booking_ven,Booking_cho,Booking_pho,Booking_meh,Booking_djs
+
+# Register your models here.
+admin.site.register(Makeup)
+admin.site.register(Decorator)
+admin.site.register(Invitation)
+admin.site.register(Catering)
+admin.site.register(Gift)
+admin.site.register(Wear)
+admin.site.register(Jewellery)
+admin.site.register(Photographer)
+admin.site.register(Hotel)
+admin.site.register(Choreographer)
+admin.site.register(Mehendi)
+admin.site.register(Dj)
+
+admin.site.register(Booking_mak)
+admin.site.register(Booking_dec)
+admin.site.register(Booking_inv)
+admin.site.register(Booking_cat)
+admin.site.register(Booking_gif)
+admin.site.register(Booking_wea)
+admin.site.register(Booking_jew)
+admin.site.register(Booking_ven)
+admin.site.register(Booking_cho)
+admin.site.register(Booking_pho)
+admin.site.register(Booking_meh)
+admin.site.register(Booking_djs)
